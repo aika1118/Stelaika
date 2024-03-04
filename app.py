68a23652c17eabab0882a0bfc393ef3a41f057e8
miya <qhfl1096@naver.com>
2024-03-04 21:19:39 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,282 @@
+# 해당 파일 실행 후 http://127.0.0.1:5000로 접속해야함
+# 파일 구조는 아래와 같아야함
+# - app.py
+# - static/
+#   - images/
+#     - your_image.jpg
+#   - css/
+#     - your_style.css
+#   - js/
+#     - your_script.js
+# - templates/
+#   - your_template.html
+
+
+from flask import Flask, render_template, jsonify, request
+from googleapiclient.discovery import build
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import datetime
+import copy
+
+app = Flask(__name__)
+
+def init_youtube_api(api_key, channel_id):
+    # YouTube API 클라이언트를 빌드합니다.
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    # 동영상 목록을 가져오는 API 요청을 만듭니다.
+    request = youtube.search().list(
+        part='snippet',
+        channelId=channel_id,
+        order='date',
+        maxResults=1  # 최근 동영상 1개만 가져오도록 설정
+    )
+
+    # API 요청을 실행하고 응답을 가져옵니다.
+    try:
+        response = request.execute()
+        print("유튜브 API 요청 성공 !")
+    except Exception as e:
+        print("유튜브 API 요청 실패 !", e)
+        exit(0)
+
+    return response
+
+def get_latest_video_data(response, member_name):
+    url = f'https://www.youtube.com/embed/{response["items"][0]["id"]["videoId"]}'
+    if not youtube_cache_dict[member_name]['data']:
+        youtube_cache_dict[member_name]['data'].append(url)
+    else:
+        youtube_cache_dict[member_name]['data'][0] = url
+    youtube_cache_dict[member_name]['time'] = datetime.datetime.now()
+    print(youtube_cache_dict[member_name]['time'])
+    return url
+
+# 카페 제목이 긴 경우 max_length에 맞추어 자르고 뒤에 ".." 붙이기
+def truncate_cafe_title(s, max_length=10):
+    if len(s) <= max_length:
+        return s
+    else:
+        return s[:max_length] + ".."
+
+def get_latest_cafe_link(url, member_name):
+    options = webdriver.ChromeOptions()
+    # headless 옵션 설정
+    options.add_argument('--headless=new')
+
+    driver = webdriver.Chrome(options=options)
+
+    # 크롤링 방지 우회
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+        "source": """
+                Object.defineProperty(navigator, 'webdriver', {
+                    get: () => undefined
+                })
+                """
+    })
+
+    driver.get(url)
+    driver.implicitly_wait(20)
+
+    # iframe 식별
+    # 스텔라이브 카페 게시글 관계없이 iframe 공통으로 보임
+    iframe = driver.find_element(By.XPATH,'//*[@id="cafe_main"]')
+
+    # iframe으로 전환
+    driver.switch_to.frame(iframe)
+
+    # 특정 url의 첫 게시글 element 얻기
+    # 모든 게시판 공통인 점 확인
+    post = driver.find_element(By.XPATH,'//*[@id="main-area"]/div[4]/table/tbody/tr[1]/td[1]/div[2]/div/a[1]')
+
+    # 해당 게시글 링크
+    cafe_link = post.get_attribute('href')
+
+    # 해당 게시글 제목
+    cafe_title = post.text
+    # 제목을 최대 글자 길이에 맞추어 자르기
+    cafe_title = truncate_cafe_title(cafe_title)
+
+    # 해당 게시글 업로드 날짜
+    cafe_time = driver.find_element(By.XPATH, '//*[@id="main-area"]/div[4]/table/tbody/tr[1]/td[3]').text
+    if ":" in cafe_time:
+        # 현재 날짜와 시간을 가져옵니다.
+        now = datetime.datetime.now()
+
+        # 날짜를 yyyy.mm.dd 형식으로 포맷팅합니다.
+        #cafe_time = now.strftime("%Y.%m.%d") + ' ' + cafe_time
+        cafe_time = now.strftime("%Y.%m.%d")
+    else:
+        cafe_time = cafe_time[:-1]
+    
+
+
+    if not cafe_link or not cafe_title or not cafe_time:
+        print("[에러] 빈 정보의 cafe_link 또는 cafe_title, cafe_time이 크롤링 되었습니다.")
+
+    # 카페 링크, 카페 제목, 업로드 날짜 순서대로 캐시 data list 삽입
+    # 캐싱된 데이터가 없는경우 데이터 최초 삽입
+    if not cafe_cache_dict[member_name]['data']:
+        cafe_cache_dict[member_name]['data'].append(cafe_link)
+        cafe_cache_dict[member_name]['data'].append(cafe_title)
+        cafe_cache_dict[member_name]['data'].append(cafe_time)
+    # 이미 캐싱된 데이터가 있지만 cache_time을 오버하여 값을 덮어쓰는 경우
+    else:
+        cafe_cache_dict[member_name]['data'][0] = cafe_link
+        cafe_cache_dict[member_name]['data'][1] = cafe_title
+        cafe_cache_dict[member_name]['data'][2] = cafe_time
+    
+    # 데이터를 새로 저장한 시간을 저장
+    cafe_cache_dict[member_name]['time'] = datetime.datetime.now()
+
+    return cafe_link, cafe_title, cafe_time
+
+
+
+def is_caching_time(cache_dict, member_name):
+    # cache된 데이터가 있는 경우
+    if cache_dict[member_name]['data']:
+        elapsed_time = (datetime.datetime.now() - cache_dict[member_name]['time']).total_seconds()
+        # 캐싱 타임 범위 안에 있는 경우
+        if elapsed_time < cache_time:
+            return True
+    
+    return False
+
+
+# http://127.0.0.1:5000로 최초 접속 시 어떤 html을 렌더링 해줄지 지정
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+# info.html GET요청 시 info.html 연결
+@app.route('/info.html')
+def info_html():
+    return render_template('info.html')
+
+# main.html GET요청 시 main.html 연결 (info.html에서 배너 클릭으로 main.html 접근 시도시 등)
+@app.route('/main.html')
+def main_html():
+    return render_template('main.html')
+
+# info-script.js에서 post 형식으로 해당 ajax 요청올 시 아래와 같이 데이터를 얻은 후 json으로 전송함
+@app.route("/get_video_data", methods=["POST"])
+def get_video_data():
+    # js 파일에서 전달한 member 변수 얻어오기 (멤버별 dictionary key값 ex.GangGi)
+    member = request.form.get('member')
+    member_name = member.split('-')[-1]
+
+    # 이전에 호출하여 cache_dict에 데이터가 있는 경우, cache_time을 초과하지 않은 경우 기존 caching 데이터 반환
+    if is_caching_time(youtube_cache_dict, member_name) == True:
+        print(member_name + ' 캐싱 데이터 반환 ! \nAPI를 호출하지 않음')
+        return jsonify({'video_link': youtube_cache_dict[member_name]['data'][0]})
+
+    channel_id = channel_id_dict[member_name]
+    response = init_youtube_api(api_key, channel_id)
+    video_link = get_latest_video_data(response, member_name)
+    return jsonify({'video_link': video_link})
+
+
+# info-script.js에서 post 형식으로 해당 ajax 요청올 시 아래와 같이 데이터를 얻은 후 json으로 전송함
+@app.route("/get_cafe_data", methods=["POST"])
+def get_cafe_data():
+    # js 파일에서 전달한 member 변수 얻어오기 (멤버별 dictionary key값 ex.GangGi)
+    member = request.form.get('member')
+    member_name = member.split('-')[-1]
+
+    # 이전에 호출하여 cache_dict에 데이터가 있는 경우, cache_time을 초과하지 않은 경우 기존 caching 데이터 반환
+    if is_caching_time(cafe_cache_dict, member_name) == True:
+        print(member_name + ' 캐싱 데이터 반환 ! \n크롤링 진행하지 않음')
+        return jsonify({
+            'cafe_link': cafe_cache_dict[member_name]['data'][0],
+            'cafe_title' : cafe_cache_dict[member_name]['data'][1],
+            'cafe_time' : cafe_cache_dict[member_name]['data'][2]
+        })
+    
+    # 네이버 카페는 게시글이 iframe안에 있어서 크롤링시 iframe url로 접근 필요
+    # 멤버별로 menuid만 다르기 때문에 해당 부분만 dictionary에서 값을 얻어와 삽입
+    url = 'https://cafe.naver.com/tteokbokk1?iframe_url=/ArticleList.nhn%3Fsearch.clubid=29424353%26search.menuid=' + cafe_menuid_dict[member_name] + '%26search.boardtype=L'
+
+    # 크롤링으로 링크, 제목, 올린날짜 얻어오기
+    cafe_link, cafe_title, cafe_time = get_latest_cafe_link(url, member_name)
+
+    return jsonify({
+        'cafe_link': cafe_link,
+        'cafe_title' : cafe_title,
+        'cafe_time' : cafe_time
+    })
+
+# channel_id 얻는법
+# 유튜브 채널 자세히 알아보기 -> 채널 공유 -> 채널 ID 복사
+channel_id_dict = {
+    'GangGi' :'UCIVFv8AiQLqM9oLHTixrNYw',
+    'Kanna' : 'UCKzfyYWHQ92z_2jUcSABM8Q',
+    'Yuni' : 'UClbYIn9LDbbFZ9w2shX3K0g',
+    'Hina' : 'UC1afpiIuBDcjYlmruAa0HiA',
+    'Masiro' : 'UC_eeSpMBz8PG4ssdBPnP07g',
+    'Tabi' : 'UCAHVQ44O81aehLWfy9O6Elw',
+    'Rize' : 'UC7-m6jQLinZQWIbwm9W-1iw'
+}
+
+#  menuid (게시판 우클릭 링크 복사로 알 수 있음)
+cafe_menuid_dict = {
+    'GangGi' :'7',
+    'Kanna' : '131',
+    'Yuni' : '134',
+    'Hina' : '149',
+    'Masiro' : '150',
+    'Tabi' : '152',
+    'Rize' : '151'
+}
+
+# ajax 요청에 따라 data에 데이터 저장, time에 데이터 저장한 시간 저장 (cache_time을 벗어났는지 비교 위함)
+member_dict = {
+    'GangGi' : {
+        'data' : [],
+        'time' : ''
+    },
+    'Kanna' : {
+        'data' : [],
+        'time' : ''
+    },
+    'Yuni' : {
+        'data' : [],
+        'time' : ''
+    },
+    'Hina' : {
+        'data' : [],
+        'time' : ''
+    },
+    'Masiro' : {
+        'data' : [],
+        'time' : ''
+    },
+    'Tabi' : {
+        'data' : [],
+        'time' : ''
+    },
+    'Rize' : {
+        'data' : [],
+        'time' : ''
+    },
+}
+
+# ajax 요청 타입별로 데이터 저장소 깊은 복사 
+youtube_cache_dict = copy.deepcopy(member_dict)
+cafe_cache_dict = copy.deepcopy(member_dict)
+
+
+
+# YouTube API 키와 채널 ID를 입력하세요.
+api_key = 'AIzaSyACvxE1jRByHukPXrNJZ86Wy9Bx9c7ehzU'
+#api_key = 'AIzaSyAcBl1hDIozS7imfQnT8GvkSZzUO1XdjLE'
+
+# 캐시 데이터 저장 시간 (초단위)
+# 해당 시간 이후 api 새로 호출하여 정보 갱신
+# 현재 6시간으로 설정
+cache_time = 6 * 3600
+
+if __name__ == '__main__':
+    #app.run(debug=True, use_reloader=False)
+    app.run(host='0.0.0.0')
---
+++ static/js/info-script.js
@@ -0,0 +1,274 @@
+// 페이지가 맨 처음 로드 될 때 동작
+window.onload = function() {
+    // main.html에서 get 형식으로 보낸 정보를 parsing
+    var urlParams = new URLSearchParams(window.location.search);
+    var selectedContent = urlParams.get('selected');
+    var selectedButton = urlParams.get('button')
+    var selectedMain = urlParams.get('main')
+
+    // get 형식으로 받은 데이터가 유효하지 않으면 기본값과 함께 홈페이지 이동
+    if (!selectedContent || !selectedButton)
+    {
+        changeActive('top-menu-gangGi')
+        showContentLeftMenu('left-menu-gangGi', 'main-content-init-GangGi');
+    }
+
+    // get 형식으로 받은 데이터에 맞게 기수 버튼 활성화, 기수 메뉴 노출
+    else
+    {
+        changeActive(selectedButton)
+        showContentLeftMenu(selectedContent, selectedMain)
+    }
+
+    // get 형식부분 주소창 깔끔하게 지우기
+    history.replaceState({}, document.title, window.location.pathname);
+};
+
+// 세로모드일 떄 왼쪽 메뉴 토글기능
+function toggleLeftMenu() {
+    var content = document.getElementById("left-menu-part");
+
+    if (content.style.display == "none" || content.style.display == "") 
+        content.style.display = "block";
+    else 
+        content.style.display = "none";
+}
+
+
+// 기수 버튼 누를 시 누른 버튼만 active 처리해주는 함수
+function changeActive(id) {
+    // 기수 버튼이 모두 같은 class로 묶여있는 상황
+    // 해당 class='active' 모두 remove 하기
+    document.querySelectorAll('.top-menu-button').forEach(function(button) {
+        button.classList.remove('active');
+    });
+
+    // 파라미터로 받은 id 속성의 버튼만 class='active' 추가
+    // 차후 css에서 active 속성을 가진 스타일을 가지게 됨
+    var clickedButton = document.getElementById(id);
+    clickedButton.classList.add('active');
+}
+
+// 기수 버튼 누를 시 왼쪽 메뉴에 맞춤형 컨텐츠 노출
+function showContentLeftMenu(id, mainInitContentId) {
+    // 왼쪽 메뉴에 표시될 컨텐츠가 같은 class로 묶여있는 상황
+    // 해당 class를 모두 보이지 않게 none 처리
+    var contents = document.getElementsByClassName('left-menu-container');
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+    }
+
+    // 파라미터로 받은 id 속성의 메뉴만 display함
+    var selectedContent = document.getElementById(id);
+    if (selectedContent) {
+        selectedContent.style.display = 'block';
+    }
+
+    // 기수 별 메인 컨텐츠 초기화면 노출
+    showContentMainPart(mainInitContentId)
+}
+
+// 왼쪽 레이아웃에서 세부메뉴 선택시 메인 컨텐츠 노출
+function showContentMainPart(id) {
+
+    // 세로모드일 때 왼쪽 메뉴 숨기기
+    mediaQuery = window.matchMedia("(orientation: portrait)");
+    if (mediaQuery.matches)
+    {
+        var menu = document.getElementById("left-menu-part");
+        menu.style.display = "none";
+    }
+
+    // 모든 컨텐츠 보이지 않게 none 처리
+    var contents = document.getElementsByClassName('main-content-init');
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+    }
+
+    var contents = document.getElementsByClassName('main-content-twitter');
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+    }
+
+    var contents = document.getElementsByClassName('main-content-cafe');
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+    }
+
+    var contents = document.getElementsByClassName('main-content-youtube');
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+
+        // main-content-youtube class에 iframe이 있는 경우 모두 제거
+        iframe = contents[i].querySelector('iframe');
+        if (iframe) 
+            iframe.parentNode.removeChild(iframe);
+    }
+
+    var contents = document.getElementsByClassName('main-content-chzzk');
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+
+        // main-content-chzzk class에 iframe이 있는 경우 모두 제거
+        iframe = contents[i].querySelector('iframe');
+        if (iframe) 
+            iframe.parentNode.removeChild(iframe);
+    }
+
+    // 파라미터로 받은 id 속성의 컨텐츠만 display함
+    var selectedContent = document.getElementById(id);
+    if (selectedContent) {
+        selectedContent.style.display = 'block';
+    }
+
+    // 왼쪽 메뉴 버튼이 눌릴 때 ajax 호출 (파이썬 파일에서 처리됨)
+    // 해당 버튼의 id가 click 되었는지 여부로 판단
+    // 데이터를 받고 .html을 통해 특정 html 위치에 삽입
+    // ` ` 문자로 html에 넣을 문장 삽입해야함 (작은 따옴표 아님)
+
+    // 특정 요소가 클릭되었을 때 반응하려면 아래와 같이 추가
+    //$(document).ready(function () {
+        //$('.getVideoLink').click(function(){\
+        //});
+    //});
+
+    // 유튜브 버튼 클릭
+    if (id.includes("main-content-youtube"))
+    {
+        // Flask 서버에 동영상 데이터를 요청하는 AJAX 호출
+        $.ajax({
+            type: 'POST',
+            url: '/get_video_data',
+            success: function (data) {
+                // 성공적으로 데이터를 받아왔을 때 처리
+                const videoLink = data.video_link;
+                const iframeCode = `<iframe src="${videoLink}" frameborder="0" allowfullscreen></iframe>`;
+                $('#' + id + ' ' + '.video-container').html(iframeCode);
+            },
+            data: {
+                member: id   
+                // 다른 파라미터들도 필요하다면 여기에 추가
+            },
+            error: function () {
+                // 오류 발생 시 처리
+                $('#' + id).text('Error fetching data from Flask server.');
+            }
+        });
+    }
+
+    // 카페 버튼 클릭
+    if (id.includes("main-content-cafe"))
+    {
+        // Flask 서버에서 데이터를 가져오는 동안 loading용 class만 노출
+        $('#' + id + ' ' + '.cafe-background-loading').show()
+        $('#' + id + ' ' + '.cafe-background').hide();
+        //$('#' + id + ' ' + '.cafe-caution-container').hide();
+        
+
+        // Flask 서버에 데이터를 요청하는 AJAX 호출
+        $.ajax({
+            type: 'POST',
+            url: '/get_cafe_data',
+            success: function (data) {
+                // 성공적으로 데이터를 받아왔을 때 처리
+                cafeLink = data.cafe_link;
+                const cafeTitle = data.cafe_title;
+                const cafeTime = data.cafe_time;
+
+                // 세로모드일 때 모바일 링크 제공하기
+                mediaQuery = window.matchMedia("(orientation: portrait)");
+                if (mediaQuery.matches)
+                    cafeLink = cafeLink.replace(/(https:\/\/)([^.]+)(.+)/, "$1m.$2$3")
+                
+                element = document.querySelector('#' + id + ' ' + '.cafe-background');
+                element.onclick = function() {
+                    window.open(cafeLink, '_blank');
+                };
+
+                $('#' + id + ' ' + '.cafe-title').html(`${cafeTitle}`);
+                $('#' + id + ' ' + '.cafe-time').html(`${cafeTime}`);
+
+                // Flask 서버에서 데이터를 가져온 후 loading용 class hide 처리, 가져온 데이터가 담긴 class 보여줌
+                $('#' + id + ' ' + '.cafe-background').show()
+                //$('#' + id + ' ' + '.cafe-caution-container').show();
+                $('#' + id + ' ' + '.cafe-background-loading').hide()
+                
+            },
+            data: {
+                member: id   
+                // 다른 파라미터들도 필요하다면 여기에 추가
+            },
+            error: function () {
+                // 오류 발생 시 처리
+                $('#' + id + ' ' + '.cafe-background-loading').hide()
+                $('#' + id).text('Error fetching data from Flask server.');
+            }
+        });
+    }
+
+    // 방송 버튼 클릭
+    if (id.includes("main-content-chzzk"))
+    {
+        channelId = "";
+        parentLink = '127.0.0.1'
+
+        switch(id)
+        {
+            case 'main-content-chzzk-GangGi':
+                channelId = 'rkdwl12';
+                break;
+            case 'main-content-chzzk-Kanna':
+                channelId = 'airikanna_stellive';
+                break;
+            case 'main-content-chzzk-Yuni':
+                channelId = 'ayatsunoyuni_stellive';
+                break;
+            case 'main-content-chzzk-Hina':
+                channelId = 'shirayukihina_stellive';
+                break;  
+            case 'main-content-chzzk-Masiro':
+                channelId = 'nenekomashiro_stellive';
+                break;  
+            case 'main-content-chzzk-Tabi':
+                channelId = 'arahashitabi_stellive';
+                break;
+            case 'main-content-chzzk-Rize':
+                channelId = 'akanelize_stellive';
+                break;   
+            default:
+                break;  
+        }
+
+        // src channel에 트위치 스트리머 id 삽입
+        // src parent에 embed될 사이트 주소 삽입 (port는 제거)
+        const iframeCode = `<iframe src="https://player.twitch.tv/?channel=${channelId}&parent=${parentLink}" frameborder="0" allowfullscreen></iframe>`;
+        $('#' + id + ' ' + '.broadcast-container').html(iframeCode);
+    }
+    
+}
+
+// 미디어 쿼리 리스너 함수
+// css에서 미디어쿼리만으로 처리하면 javascript에서 동적으로 display = "none" 바뀐 이후 가로모드될 때 display = "block"으로 바뀌지 않아 처리
+function handleMediaQueryChange(mq) {
+    if (mq.matches) 
+    {
+        // 가로모드로 변경될 시 왼쪽 메뉴 표시
+        var menu = document.getElementById("left-menu-part");
+        menu.style.display = "block";
+    }
+    else
+    {
+        // 세로모드로 변경될 시 왼쪽 메뉴 가리기
+        var menu = document.getElementById("left-menu-part");
+        menu.style.display = "none";
+    }
+
+}
+
+// 가로 모드를 감지하는 미디어 쿼리
+var mediaQuery = window.matchMedia("(orientation: landscape)");
+
+// 미디어 쿼리가 변경될 때 호출되는 함수 연결
+mediaQuery.addListener(handleMediaQueryChange);
+
+
